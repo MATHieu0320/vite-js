daef0f8f0745239f3f8ae855490d2b6bbe390c18
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-06-24 20:42:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import confetti from "canvas-confetti";
+import zoro from "./zoro.jpeg";
+import "./style.scss";
+import { setupCounter } from "./counter";
+confetti();
+setupCounter(document.querySelector("button"));
+console.log(zoro, css);
