632f778fa9c8f7443c26007f79b9112988a4bf54
Mathieu <97351983+MATHieu0320@users.noreply.github.com>
2024-07-10 17:56:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 import confetti from "canvas-confetti";
 import zoro from "./zoro.jpeg";
 import "./style.scss";
-import { setupCounter } from "./counter";
-confetti();
-setupCounter(document.querySelector("button"));
-console.log(zoro, css);
+
+const btn = document.querySelector("button");
+
+import { setupCounter } from "./counter.js";
+//
+// setupCounter(document.querySelector("button"));
+btn.addEventListener("click", () => {
+  confetti();
+});
+
+import("./counter.js").then((func) => {
+  func.setupCounter(document.querySelector("button"));
+});
+console.log(import.meta);
